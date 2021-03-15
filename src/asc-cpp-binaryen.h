56bb40af9b2d7bfe8e8acf571ce3e124e4633766
Turtle1331 <turtle1331@github.com>
2021-03-14 20:55:10 -0700

    Separate Binaryen and Wasmtime dependencies into libraries

---
+++ src/asc-cpp-binaryen.h
@@ -0,0 +1,14 @@
+#ifndef ASC_CPP_BINARYEN_H
+#define ASC_CPP_BINARYEN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int asc_cpp_binaryen(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/asc-cpp-wasmtime.h
@@ -0,0 +1,14 @@
+#ifndef ASC_CPP_WASMTIME_H
+#define ASC_CPP_WASMTIME_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int asc_cpp_wasmtime(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
